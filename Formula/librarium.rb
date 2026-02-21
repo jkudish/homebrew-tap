b1247b02cee79b40116330459babe1de25197eed
Joey Kudish <joachim.kudish@gmail.com>
2026-02-21 17:30:53 +0700

    Update librarium to v0.1.0 with real SHA256 hashes

--- Formula/librarium.rb
+++ Formula/librarium.rb
@@ -7,20 +7,20 @@ class Librarium < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/jkudish/librarium/releases/download/v0.1.0/librarium-macos-arm64"
-      sha256 "PLACEHOLDER_SHA256_MACOS_ARM64"
+      sha256 "597e63931a0798076fed730f96f395643dfad5c35f7bc3af7312efb575b6ce8a"
     else
       url "https://github.com/jkudish/librarium/releases/download/v0.1.0/librarium-macos-x64"
-      sha256 "PLACEHOLDER_SHA256_MACOS_X64"
+      sha256 "4b015dd10f727fd9a8052fd734a9e284ff551b2aabc3808786cea481fa1ce99e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/jkudish/librarium/releases/download/v0.1.0/librarium-linux-arm64"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      sha256 "f52b496657023d0bc82bf4dd39087f319f83b1eb18eaa16318ca2a11897adc94"
     else
       url "https://github.com/jkudish/librarium/releases/download/v0.1.0/librarium-linux-x64"
-      sha256 "PLACEHOLDER_SHA256_LINUX_X64"
+      sha256 "d84321673a249ba0d6ab11c34fc7164bb60931b0ae390d8db5d6d220bbefde33"
     end
   end
 
